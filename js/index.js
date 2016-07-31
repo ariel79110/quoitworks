05e434d1ccfd2686f41cb411be82f0eb13598bc4
Chen Ying Chu <chenyingchu@ChentekiMacBook-Air.local>
2016-07-31 17:43:22 +0800

    website pushed

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+// 等頁面loading完成後執行
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        // 中間頁面
+        $('.rightnav').toggleClass('rightnav01');
+        // menu頁面
+        $('.b').toggleClass('b-class');  
+    })
+})
+
+
+  
+  
+  
